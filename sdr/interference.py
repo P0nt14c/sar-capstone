f6d1d77fe44eebfcb79388b5ad79560fd8670459
sao7357 <60437150+sao7357@users.noreply.github.com>
2023-12-06 19:43:11 -0500

    Update interference.py

--- sdr/interference.py
+++ sdr/interference.py
@@ -1,7 +1,8 @@
 import numpy as np
 import uhd
+import time
 
-def generate_noise_segment(center_freq, bandwidth, segment_duration, tx_gain, usrp):
+def generate_and_transmit_noise(usrp, center_freq, bandwidth, tx_gain, segment_duration):
     # Generate random noise for a segment
     num_samples = int(bandwidth * segment_duration)
     noise_signal = np.random.normal(size=num_samples) + 1j * np.random.normal(size=num_samples)
@@ -9,17 +10,22 @@ def generate_noise_segment(center_freq, bandwidth, segment_duration, tx_gain, us
     # Transmit noise segment
     usrp.send_waveform(noise_signal, segment_duration, center_freq)
 
-def generate_continuous_noise(center_freq, bandwidth, total_duration, tx_gain):
+def continuous_noise_transmission(center_freq, bandwidth, total_duration, tx_gain, segment_duration):
     usrp = uhd.usrp.MultiUSRP("type=b200")
     usrp.set_tx_rate(bandwidth)
     usrp.set_tx_freq(float(center_freq))
     usrp.set_tx_gain(tx_gain)
 
-    segment_duration = 0.1  # Duration of each noise segment in seconds
-    num_segments = int(total_duration / segment_duration)
+    end_time = time.time() + total_duration
+    while time.time() < end_time:
+        generate_and_transmit_noise(usrp, center_freq, bandwidth, tx_gain, segment_duration)
 
-    for _ in range(num_segments):
-        generate_noise_segment(center_freq, bandwidth, segment_duration, tx_gain, usrp)
+# Parameters
+center_freq = 5935000000  # 5.935 GHz
+bandwidth = 56e6  # 56 MHz
+tx_gain = 20
+total_duration = 5  # 5 seconds
+segment_duration = 0.1  # 100 milliseconds
 
-# Example call for 5 seconds of noise jamming
-generate_continuous_noise(5935000000, 56, 5, 20)
+# Start continuous noise transmission
+continuous_noise_transmission(center_freq, bandwidth, total_duration, tx_gain, segment_duration)
